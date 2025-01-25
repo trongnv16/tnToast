92772e8ffcd90ddaf0251926d98fd3908ba315b8
trongnv <nvtrong1615@gmail.com>
2025-01-25 15:18:16 +0700

    udate

--- tn_toast.js
+++ tn_toast.js
@@ -1,20 +1,17 @@
 /**
  * Toast notification
- * 
- * Reduce datatable rendering time by rendering basic fields the first time 
- * and rendering fields that take a long time to compute from the server the second time
- * 
- * 
+ *
  * @author TrongNV
  * https://github.com/trongnv16
- * 
- * @param {object} option 
+ *
+ * @param {object} option
  * elementId datatable Id
  * urlLoadMore url load more datatable
  * rowNameGetId field to compare with load more
- * 
+ *
  */
- TNToast = function (content, type) {
+var tntoastIx = 1;
+TNToast = function (content, type) {
     if (content == undefined) {
         content = "Notification";
     }
@@ -22,15 +19,15 @@
     let colorCode;
     switch (type) {
         case "error":
-            colorCode = "#da4545";
+            colorCode = "#e96b6b";
             break;
 
         case "success":
-            colorCode = "#4ab84a";
+            colorCode = "#73d473";
             break;
 
         default:
-            colorCode = "#4ab84a";
+            colorCode = "#73d473";
             break;
     }
 
@@ -39,12 +36,11 @@
     let bodyElement;
 
     if (document.getElementsByClassName("tn-toast-zone").length) {
-        
     } else {
         elementToastZone = document.createElement("div");
         elementToastZone.className = "tn-toast-zone";
         bodyElement = document.getElementsByTagName("body")[0];
-        bodyElement.prepend(elementToastZone)
+        bodyElement.prepend(elementToastZone);
     }
 
     elementToastZone = document.getElementsByClassName("tn-toast-zone")[0];
@@ -52,14 +48,27 @@
     elementToastItem = document.createElement("div");
     elementToastItem.className = "tn-toast-item";
 
-    let TNToastElement = '<div class="tn-toast" style="background:' + colorCode + ';"><div class="tn-toast-header"><button onclick="closeTnToast(this)" class="tn-toast-btn-close" type="button"><span aria-hidden="true">&times;</span></button></div><div class="tn-toast-body">' + content + '</div></div>';
+    let TNToastElement = "";
+    TNToastElement += '<div class="tn-toast-' + tntoastIx + '">';
+    TNToastElement += '<style type="text/css">@keyframes append-animate{from{transform:translateX(-50%);opacity:0}to{transform:translateX(0);opacity:1}}.tn-toast{animation:append-animate .2s linear}</style>';
+    TNToastElement +=
+        '<div class="tn-toast" style="background:' +
+        colorCode +
+        ';"><div class="tn-toast-header"><button onclick="closeTnToast(' + tntoastIx + ')" class="tn-toast-btn-close" type="button"><span aria-hidden="true">&times;</span></button></div><div class="tn-toast-body">' +
+        content +
+        "</div></div>";
+    TNToastElement += '</div>';
 
     elementToastItem.innerHTML = TNToastElement;
-    elementToastZone.prepend(elementToastItem)
-}
+    elementToastZone.prepend(elementToastItem);
 
-closeTnToast = function (ele) {
-    let parentElement = document.getElementsByClassName("tn-toast-btn-close")[0].closest(".tn-toast");
+    setTimeout(() => {
+        elementToastItem.remove();
+    }, 2500);
+    tntoastIx++;
+};
+
+closeTnToast = function (index) {
+    let parentElement = document.getElementsByClassName("tn-toast-" + index)[0];
     parentElement.remove();
-}
-
+};
